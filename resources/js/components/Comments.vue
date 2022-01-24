3628ded81cd855b16e4d143585f6e0ca29d5558a
gui1998 <guilhermeissom@outlook.com>
2022-01-24 15:03:52 -0400

    fix comments modal

--- resources/js/components/Comments.vue
+++ resources/js/components/Comments.vue
@@ -85,7 +85,7 @@
         aria-labelledby="addNew"
         aria-hidden="true"
       >
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" v-show="!editmode">
@@ -112,7 +112,7 @@
               <div class="modal-body">
                 <div class="form-group">
                   <label>Name</label>
-                  <input
+                  <textarea
                     v-model="form.text"
                     type="text"
                     name="text"
