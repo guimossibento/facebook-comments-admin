a321ecad457bf56a28f0f14ad10365a69a2c9bc9
gui1998 <guilhermeissom@outlook.com>
2022-02-18 16:22:02 -0400

    fix

--- app/Interface/Http/Controllers/Api/CommentLogController.php
+++ app/Interface/Http/Controllers/Api/CommentLogController.php
@@ -2,72 +2,79 @@
 
 namespace App\Interface\Http\Controllers\Api;
 
+use App\Domain\Models\CommentLog;
+use App\Domain\Models\FacebookAccount;
 use App\Domain\Services\CommentLogDomainService;
-use App\Infrastructure\Events\CommentLogEvent;
 use App\Interface\Http\Controllers\AController;
-use  App\Domain\Models\Comment;
-use  App\Domain\Models\CommentLog;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class CommentLogController extends AController
 {
-  public function __construct(protected CommentLogDomainService $service)
-  {
-  }
-
-  /**
-   * @return Response
-   */
-  public function index()
-  {
-    return $this->sendResponse($this->service->paginate(), '');
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param Request $request
-   * @return Response
-   */
-  public function store(Request $request)
-  {
-    $data = $this->service->store($request->all());
+	public function __construct(protected CommentLogDomainService $service)
+	{
+	}
 	
-    return $this->sendResponse($data, '');
-  }
-
-  /**
-   * Display the specified resource.
-   *
-   * @param CommentLog $commentLog
-   * @return Response
-   */
-  public function show(CommentLog $commentLog)
-  {
-    return $this->sendResponse($commentLog, str_replace('DomainService', '', class_basename($this->service) . " Show"));
-  }
-
-  /**
-   * @param CommentLog $commentLog
-   * @return bool|null
-   */
-  public function destroy(CommentLog $commentLog): ?bool
-  {
-    return $this->service->delete($commentLog);
-  }
-
-  public function list(): \Illuminate\Database\Eloquent\Collection|array
-  {
-    return $this->service->list();
-  }
+	/**
+	 * @return Response
+	 */
+	public function index()
+	{
+		return $this->sendResponse($this->service->paginate(), '');
+	}
+	
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+		$facebook_account = FacebookAccount::query()
+			->where('login', $request->get('facebook_account_login'))->first();
+		
+		$data = $request->all();
+		unset($data["facebook_account_login"]);
+		
+		$data = array_merge($data, ["facebook_account_id" => $facebook_account->id]);
+		
+		$stored = $this->service->store($data);
+		
+		return $this->sendResponse($stored, '');
+	}
+	
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param CommentLog $commentLog
+	 * @return Response
+	 */
+	public function show(CommentLog $commentLog)
+	{
+		return $this->sendResponse($commentLog, str_replace('DomainService', '', class_basename($this->service) . " Show"));
+	}
+	
+	/**
+	 * @param CommentLog $commentLog
+	 * @return bool|null
+	 */
+	public function destroy(CommentLog $commentLog): ?bool
+	{
+		return $this->service->delete($commentLog);
+	}
+	
+	public function list(): \Illuminate\Database\Eloquent\Collection|array
+	{
+		return $this->service->list();
+	}
 	
 	/**
 	 * @return bool|null
 	 */
-  public function destroyAll(): ?bool
-  {
-    return $this->service->deleteAll();
-  }
+	public function destroyAll(): ?bool
+	{
+		return $this->service->deleteAll();
+	}
 }
 
